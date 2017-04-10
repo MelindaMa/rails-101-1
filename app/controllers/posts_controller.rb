06424b870b404ed8683e40ce64330b92a84aadff
“马静” <melindama@me.com>
2017-04-10 16:36:04 +0800

    implement posts#edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,8 @@
 class PostsController < ApplicationController
-
-  before_action :authenticate_user!, :only => [:new, :create]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
 
   def index
-      @posts = current_user.posts
+    @posts = current_user.posts
   end
 
   def new
@@ -14,13 +13,15 @@ class PostsController < ApplicationController
   def edit
     @group = Group.find(params[:id])
     @post = Post.find(params[:id])
+
   end
 
   def update
     @group = Group.find(params[:id])
     @post = Post.find(params[:id])
-    if @post.update(group_params)
-       redirect_to groups_path, notice: "Update Success"
+
+    if @post.update(post_params)
+       redirect_to account_posts_path, notice: "Update Success"
     else
        render :edit
     end
@@ -29,21 +30,20 @@ class PostsController < ApplicationController
   def create
     @group = Group.find(params[:group_id])
     @post = Post.new(post_params)
-    @post.group = @group
-    @post.user = current_user
 
     if @post.save
-      redirect_to group_path(@group)
+      redirect_to account_posts_path(@post)
     else
       render :new
     end
   end
 
   def destroy
+    @group = Group.find(params[:id])
     @post = Post.find(params[:id])
     @post.destroy
     flash[:alert] = "Post deleted"
-    redirect_to groups_path
+    redirect_to account_posts_path
   end
 
   private
