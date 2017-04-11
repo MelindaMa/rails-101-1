2e719a30258bc29051460fedd8f2147b6629b77c
“马静” <melindama@me.com>
2017-04-11 09:38:44 +0800

    implement my posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,25 +2,25 @@ class PostsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
 
   def index
-    @posts = current_user.posts
+    @ posts = current_user.posts
   end
 
   def new
-    @group = Group.find(params[:group_id])
-    @post = Post.new
+    @ group = Group.find(params[:group_id])
+    @ post = Post.new
   end
 
   def edit
-    @group = Group.find(params[:id])
-    @post = Post.find(params[:id])
+    @ group = Group.find(params[:id])
+    @ post = Post.find(params[:id])
 
   end
 
   def update
-    @group = Group.find(params[:id])
-    @post = Post.find(params[:id])
+    @ group = Group.find(params[:id])
+    @ post = Post.find(params[:id])
 
-    if @post.update(post_params)
+    if @ post.update(post_params)
        redirect_to account_posts_path, notice: "Update Success"
     else
        render :edit
@@ -28,20 +28,20 @@ class PostsController < ApplicationController
   end
 
   def create
-    @group = Group.find(params[:group_id])
-    @post = Post.new(post_params)
+    @ group = Group.find(params[:group_id])
+    @ post = Post.new(post_params)
 
-    if @post.save
-      redirect_to account_posts_path(@post)
+    if @ post.save
+      redirect_to account_posts_path(@ post)
     else
       render :new
     end
   end
 
   def destroy
-    @group = Group.find(params[:id])
-    @post = Post.find(params[:id])
-    @post.destroy
+    @ group = Group.find(params[:id])
+    @ post = Post.find(params[:id])
+    @ post.destroy
     flash[:alert] = "Post deleted"
     redirect_to account_posts_path
   end
